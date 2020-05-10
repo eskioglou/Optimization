7786a66d4bc9c1c635fe0bf67269d10f38a2fb5a
Maria Eskioglou <37123070+eskioglou@users.noreply.github.com>
2020-05-10 22:38:24 +0300

    Small Changes #5

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ choice1 = pulp.LpVariable('choice1', lowBound=1)
 choice2 = pulp.LpVariable('choice2', lowBound=1)
 choice3 = pulp.LpVariable('choice3', lowBound=1)
 choice4 = pulp.LpVariable('choice4', lowBound=1)
-choice5 = pulp.LpVariable('choice5', lowBound=1)
+choice5 = pulp.LpVariable('choice5', lowBound=3)
 
 choices = [choice1, choice2, choice3, choice4, choice5]
 calories = [65, 328, 216, 137, 6.9]
@@ -25,15 +25,13 @@ prob += pulp.lpSum([vitA[i]*choices[i] for i in range(len(choices))]) >= 700
 prob += pulp.lpSum([protein[i]*choices[i] for i in range(len(choices))]) >= 50
 
 prob.solve()
-#print(pulp.LpStatus[model.status])
 
-print("Servings of apples = {}".format(choice1.varValue))
-print("Servings of chicken = {}".format(choice2.varValue))
-print("Servings of brown rice = {}".format(choice3.varValue))
-print("Servings of chips = {}".format(choice4.varValue))
-print("Servings of spinach = {}".format(choice5.varValue))
+print("Servings of apples = {}".format(choice1.value()))
+print("Servings of chicken = {}".format(choice2.value()))
+print("Servings of brown rice = {}".format(choice3.value()))
+print("Servings of chips = {}".format(choice4.value()))
+print("Servings of spinach = {}".format(choice5.value()))
 
-total_cost = pulp.value(prob.objective)
 print("\nThe total cost is ${} for us to get the required daily nutrients".format(round(total_cost, 5)))
 
 def totalNutrients(choice1, choice2, choice3, choice4, choice5):
@@ -55,3 +53,5 @@ def totalNutrients(choice1, choice2, choice3, choice4, choice5):
     print("Total Protein: {}/50".format(round(totalProt)))
 
 totalNutrients(choice1.varValue, choice2.varValue, choice3.varValue, choice4.varValue, choice5.varValue)
+print("\n")
+print("Status:", pulp.LpStatus[prob.status])
